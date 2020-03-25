fd878324e10b1e59f985ab377a2e6c94d7283ba3
chryl <cyl5yy412@gmail.com>
2020-03-25 21:09:27 +0800

    init

---
+++ src/com/chryl/collection/Person.java
@@ -0,0 +1,67 @@
+package com.chryl.collection;
+
+import java.util.Set;
+import java.util.TreeMap;
+
+/**
+ * treeMap的排序功能
+ * Created by Chr.yl on 2020/3/25.
+ *
+ * @author Chr.yl
+ */
+public class Person implements Comparable<Person> {
+
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    /**
+     * TODO重写compareTo方法实现按年龄来排序
+     */
+    @Override
+    public int compareTo(Person o) {
+
+        // TODO Auto-generated method stub
+        if (this.age > o.getAge()) {
+            return 1;
+        } else if (this.age < o.getAge()) {
+            return -1;
+        }
+        return age;
+    }
+    
+
+    public static void main(String[] args) {
+        TreeMap<Person, String> pdata = new TreeMap<>();
+        pdata.put(new Person("张三", 30), "zhangsan");
+        pdata.put(new Person("李四", 20), "lisi");
+        pdata.put(new Person("王五", 10), "wangwu");
+        pdata.put(new Person("小红", 5), "xiaohong");
+        // 得到key的值的同时得到key所对应的值
+        Set<Person> keys = pdata.keySet();
+        for (Person key : keys) {
+            System.out.println(key.getAge() + "-" + key.getName());
+
+        }
+    }
+}
--- src/com/chryl/po/User.java
+++ src/com/chryl/po/User.java
@@ -1,7 +1,10 @@
 package com.chryl.po;
 
+import java.util.Comparator;
+import java.util.TreeSet;
+
 /**
- * Created by Chryl on 2020/3/20.
+ * @author Chr.yl
  */
 public class User {
     private String uname;
@@ -9,6 +12,12 @@ public class User {
 
     public User() {
     }
+
+    public User(String uname, String upass) {
+        this.uname = uname;
+        this.upass = upass;
+    }
+
 
     public String getUname() {
         return uname;
@@ -24,5 +33,44 @@ public class User {
 
     public void setUpass(String upass) {
         this.upass = upass;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "uname='" + uname + '\'' +
+                ", upass='" + upass + '\'' +
+                '}';
+    }
+
+
+    /**
+     * 定义比较器,在compare内部进行设置比较规则即可
+     *
+     * @author Chr.yl
+     *         升序比较器，降序只要将b1,b2换个顺序即可
+     */
+    public static class MyComparator implements Comparator {
+
+        @Override
+        public int compare(Object o1, Object o2) {
+            User b1 = (User) o1;
+            User b2 = (User) o2;
+            return b1.getUname().length() - b2.getUname().length();
+        }
+    }
+
+
+    public static void main(String[] args) {
+
+        TreeSet<User> treeset = new TreeSet<>(new MyComparator());
+
+        treeset.add(new User("动物世界111", "30"));
+        treeset.add(new User("动物世界", "30"));
+        treeset.add(new User("动物世界2", "30"));
+
+        System.out.println(treeset);
+
+
     }
 }
